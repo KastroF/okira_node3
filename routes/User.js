717695f4bae3a00674601e56d38b1dc615763457
KastroF <nkastrro@gmail.com>
2025-10-21 12:52:31 +0100

    On se loose 2

--- routes/User.js
+++ routes/User.js
@@ -12,7 +12,6 @@ router.post("/signup", userCtrl.signUp);
 router.post("/signin", userCtrl.signIn);
 router.post("/verifycode", auth, userCtrl.verifyCode);
 router.get("/checkuser", auth, userCtrl.checkUser);
-//router.post("/sendcode",auth, userCtrl.sendCode);
 router.post("/addphoto", auth, multer, userCtrl.addPhoto);
 router.get("/ondelete", auth, userCtrl.onDelete); 
 
